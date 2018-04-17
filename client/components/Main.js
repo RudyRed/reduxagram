bf9895f36bfec2d32652c3f9075492c870d6df46
RudyRed <willgross94@gmail.com>
2018-04-17 14:24:30 -0400

    Basic routing between PhotoGrid and Single set up

--- client/components/Main.js
+++ client/components/Main.js
@@ -7,8 +7,9 @@ const Main = React.createClass({
     return (
       <div>
         <h1>
-          <Link to='/'>Reduxstagram</Link>
+          <Link to="/">Reduxstagram</Link>
         </h1>
+        {React.cloneElement(this.props.children, this.props)}
       </div>
     )
   }
