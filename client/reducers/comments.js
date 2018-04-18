dbeb74462b9c1cc24b6530064d17ca21b5643ec0
RudyRed <willgross94@gmail.com>
2018-04-18 16:14:06 -0400

    Adding and removing comments enabled

--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,5 +1,29 @@
+const postComments = function (state = [], action) {
+  switch(action.type) {
+    case 'ADD_COMMENT':
+      const newComment = {
+        user: action.author,
+        text: action.comment,
+      }
+
+      return [...state, newComment];
+    case 'REMOVE_COMMENT':
+      const { i } = action;
+
+      return [...state.slice(0, i), ...state.slice(i + 1)];
+    default:
+      return state;
+  }
+
+}
+
 const comments = function (state = [], action) {
-  console.log(state, action);
+  if (typeof action.postId !== undefined) {
+    return {
+      ...state,
+      [action.postId]: postComments(state[action.postId], action),
+    }
+  }
   return state;
 }
 
